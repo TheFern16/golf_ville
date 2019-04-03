7d985108417c8d4ade017cc0572bf983d4c6bbc9
Matthew Fernandez CTR <matt.fernandez@NOMS.MUGU>
2019-04-03 11:32:14 -0700

    hello world on react app

---
+++ golf_ville_react/src/Game/Board/Board.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
+import './Board.css';
+
+class Board extends React.Component {
+	render() {
+		return (
+			<h1>Hello World</h1>
+		)
+	}
+};
+
+export default Board;
