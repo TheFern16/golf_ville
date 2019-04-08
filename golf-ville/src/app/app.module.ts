a2cb335d1b605ccded6c04e4f2f84e69680ff539
Matthew Fernandez CTR <matt.fernandez@NOMS.MUGU>
2019-04-08 10:20:13 -0700

    render landing page on init

--- golf-ville/src/app/app.module.ts
+++ golf-ville/src/app/app.module.ts
@@ -9,10 +9,8 @@ import { LandingComponent } from './landing/landing.component'
 import { TitleistComponent } from './brands/titleist/titleist.component';
 
 const applicationRoutes: Routes = [
-	// { path: 'titleist', component: Titleist },
-	// { path: '', redirectTo: '/landing', pathMatch: 'full'},
-	// { path: '**', component: 404 }
-
+  { path: 'titleist', component: TitleistComponent },
+  { path: '**', component: LandingComponent }
 ]
 
 @NgModule({
@@ -23,8 +21,8 @@ const applicationRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-    // RouterModule.forRoot(applicationRoutes)
+    AppRoutingModule,
+    RouterModule.forRoot(applicationRoutes)
   ],
   providers: [],
   bootstrap: [
