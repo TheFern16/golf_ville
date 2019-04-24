03562b3e9755a686b3a75bf0a7d8c24e8d0a9572
Matt <Matt@Homes-MacBook-Air.local>
2019-04-23 18:44:57 -0700

    ngfor let club titleist club selection

--- golf-ville/src/app/brands/titleist/titleist.component.ts
+++ golf-ville/src/app/brands/titleist/titleist.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./titleist.component.css']
 })
 export class TitleistComponent {
-	clubs = ['Drivers', 'Fairways', 'Hybrids', 'Irons', 'Putters'];
+	// clubs = ['Drivers', 'Fairways', 'Hybrids', 'Irons', 'Putters'];
+	clubs = [
+		{
+			setName: 'AP1',
+			setPath: '/assets/ap1.jpeg'
+		},
+		{
+			setName: 'AP2',
+			setPath: '/assets/ap2.jpeg'
+		},
+		{
+			setName: 'AP3',
+			setPath: '/assets/ap3.jpeg'
+		}
+	]
 };
